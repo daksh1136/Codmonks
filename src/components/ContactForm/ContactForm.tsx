7a1a0bbff1a94acac6672d61b3b44473728da631
Mahesh Sharma <mahesh1988.2009@gmail.com>
2025-07-06 18:18:38 +0530

    updated form post url

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -14,7 +14,7 @@ export default function ContactForm() {
 
 
     try {
-      const response = await fetch("https://script.google.com/macros/s/AKfycbz679UWmOAWOHSYXPop0abBblpBY7Fn4_mZ5aXgvOzKk7tWskw81uWa-AuDenP12I22/exec", {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbzcvrEJAj98eJ2k9FAyfZBhLpSLMdZ8ivedZjU8mdk4FKqwRjI2TRzMwIa2BexAM92-/exec", {
         method: "POST",
         body: formData,
       });
