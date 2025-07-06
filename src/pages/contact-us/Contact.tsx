d694e82722b6838109ab01cc9a10a3b19498cec9
Mahesh Sharma <mahesh1988.2009@gmail.com>
2025-07-06 18:45:08 +0530

    removed unused properties

--- src/pages/contact-us/Contact.tsx
+++ src/pages/contact-us/Contact.tsx
@@ -1,66 +1,7 @@
 import React from "react";
-import { useState } from "react";
 import ContactForm from "../../components/ContactForm/ContactForm";
 
-type FormData = {
-    name: string;
-    email: string;
-    phone: string;
-    message: string;
-};
-
-type FormErrors = {
-    name?: string;
-    email?: string;
-    phone?: string;
-};
-
-
 const Contact: React.FC = () => {
-    const [formData, setFormData] = useState<FormData>({
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
-    });
-
-    const [errors, setErrors] = useState<FormErrors>({});
-    const [successMsg, setSuccessMsg] = useState<string>("");
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const validate = (): FormErrors => {
-        const newErrors: FormErrors = {};
-        if (!formData.name.trim()) newErrors.name = "Name is required";
-        if (!formData.email.trim()) newErrors.email = "Email is required";
-        if (!formData.phone.trim()) newErrors.phone = "Phone is required";
-        return newErrors;
-    };
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const validationErrors = validate();
-        if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-            return;
-        }
-
-        setErrors({});
-        setSuccessMsg("Your details has been sent successfully!");
-        setFormData({
-            name: "",
-            email: "",
-            phone: "",
-            message: "",
-        });
-
-        setTimeout(() => {
-            setSuccessMsg("");
-        }, 5000);
-    };
-
     return (
         <>
             <section className="bg-gradient-to-r from-indigo-600 to-purple-500 text-white py-20">
