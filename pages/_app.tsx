9e79fa3072c0edfc2081be7a602de1aff277feb5
Mahesh Sharma <mahesh1988.2009@gmail.com>
2025-09-07 14:27:23 +0530

    revert ga to gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import TagManager from "react-gtm-module";
 
 // Import the Font Awesome CSS
 import '@fortawesome/fontawesome-svg-core/styles.css'; // Import Font Awesome CSS
@@ -8,8 +9,12 @@ import '@fortawesome/fontawesome-svg-core/styles.css'; // Import Font Awesome CS
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically
 import Layout from '../components/Layout';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-5KZMPH39" });
+  }, []);
   return (<Layout>
     <Component {...pageProps} />
   </Layout>);
