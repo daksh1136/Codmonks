00a5845d8a43dd7fdd03151c0a9444d1b83137b3
Mahesh Sharma <mahesh1988.2009@gmail.com>
2025-09-07 14:11:11 +0530

    change gtags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-MTZEF48F3Q" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MTZEF48F3Q');
+          `}
+        </Script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
