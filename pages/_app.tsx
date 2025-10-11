b1c6b6a15e2c3c74dde82d9bd5a288f548fc8eef
Mahesh Sharma <mahesh1988.2009@gmail.com>
2025-10-11 08:11:18 +0530

    moved speedinsight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,12 +10,16 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically
 import Layout from '../components/Layout';
 import { useEffect } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: "GTM-5KZMPH39" });
   }, []);
-  return (<Layout>
-    <Component {...pageProps} />
-  </Layout>);
+  return (<>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    <SpeedInsights />
+  </>);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -20,7 +19,6 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
-        <SpeedInsights />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `
           {
             "@context": "https://schema.org",
