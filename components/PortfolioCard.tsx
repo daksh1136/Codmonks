fbf319e251f70b033d21d9e434e6acef6296f666
Mahesh Sharma <mahesh1988.2009@gmail.com>
2026-03-17 13:35:16 +0530

    added portfolio

---
+++ components/PortfolioCard.tsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+type PortfolioProps = {
+  id: number;
+  title: string;
+  subTitle: string;
+  description: string;
+  keyFeature?: string[];
+  tools?: string[];
+  icon: string;
+  image: string;
+  techStack?: string;
+  link?: string;
+  caseStudyLink?: string;
+  catagory: "mobile" | "web";
+};
+
+type PortfolioCardProps = {
+  portfolio: PortfolioProps;
+};
+
+const PortfolioCard = (props: PortfolioCardProps) => {
+  const { portfolio } = props;
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <div
+      key={portfolio?.id}
+      className="relative portfolio-card bg-white rounded-xl hover:shadow-lg ring-1 mt-4 ring-gray-200 overflow-hidden"
+      data-category={portfolio?.catagory}
+    >
+      <div className="bg-gray-50">
+        <div className="px-5 pt-5">
+          <img
+            alt=""
+            className="rounded-md mx-auto h-[50px]"
+            src={"/assets/images/portfolio/" + portfolio?.icon}
+          />
+          <p className="text-center mt-3 uppercase font-bold">
+            {portfolio?.title}&nbsp;
+          </p>
+          <p className="text-center mt-3 uppercase text-sm">
+            {portfolio?.subTitle}&nbsp;
+          </p>
+          <div className="min-h-48 flex items-center justify-center mt-3">
+            <Image
+              height={291}
+              width={portfolio?.catagory === "mobile" ? 182 : 407}
+              alt=""
+              className="rounded-md"
+              src={"/assets/images/portfolio/" + portfolio?.image}
+            />
+          </div>
+        </div>
+        <div className="p-6 bg-white">
+          <p className="text-gray-600 mb-2 line-clamp-2 font-semibold">
+            {portfolio?.description}{" "}
+          </p>
+          <p
+          className="cursor-pointer mb-4 text-blue-600"
+          onClick={() => setShowInfo(true)}>
+            Read more...
+          </p>
+          <p className="text-red-600 text-xs">{portfolio?.techStack} </p>
+        </div>
+      </div>
+      <div className={"absolute top-0 w-full h-full bg-gray-900 bg-opacity-75 text-white p-6 overflow-auto" + (!showInfo ? ' left-[100%]' : ' left-0')}>
+        <p>
+          {portfolio?.description}
+        </p>
+        {
+          portfolio?.keyFeature &&
+          <ul className="my-5">
+            <li className="font-bold mt-3">Key Features:</li>
+            {portfolio?.keyFeature?.map((feature, index) => (
+              <li key={index} className="ml-4">
+                {feature}
+              </li>
+            ))}
+          </ul>
+        }
+        {
+          portfolio?.tools &&
+          <ul className="my-5">
+            <li className="font-bold mt-3">Tech & Tools:</li>
+            {portfolio?.keyFeature?.map((feature, index) => (
+              <li key={index} className="ml-4">
+                {feature}
+              </li>
+            ))}
+          </ul>
+        }
+        <button
+          className="border px-3 py-1 mt-3 w-full rounded-lg bg-white cursor-pointer mb-4 text-black"
+          onClick={() => setShowInfo(false)}>
+            Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioCard;
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,7 +1,37 @@
 import Head from "next/head";
 import Image from "next/image";
+import { portfolioList } from '../../data/portfolios';
+import { useRef, useState } from "react";
+import PortfolioCard from "@/components/PortfolioCard";
+
+type selectOptionProps = {
+    title: string,
+    value: string
+}
 
 const Portfolio = () => {
+
+    const [ filteredList, setFilteredList ] = useState(portfolioList);
+    const inputRef = useRef<HTMLSelectElement | null>(null);
+    const portfolioOptions = [
+        {title: 'All', value: 'all'},
+        {title: 'Mobile', value: 'mobile'},
+        {title: 'Web', value: 'web'},
+        {title: 'Frontend', value: 'frontend'},
+        {title: 'CRM', value: 'crm'},
+        {title: 'Unity', value: 'unity'},
+        {title: 'CMS', value: 'cms'},
+        {title: 'Others', value: 'others'},
+    ];
+
+    const changeHandler = () => {
+        if(inputRef?.current?.value !== 'all'){
+            setFilteredList(portfolioList.filter((item) => item?.catagory === inputRef?.current?.value));
+        }
+        else {
+            setFilteredList(portfolioList);
+        }
+    };
 
     return (
         <>
@@ -49,107 +79,19 @@ const Portfolio = () => {
                             At CodMonks, we combine <strong>technical excellence with design thinking</strong> to craft seamless user experiences and business-driven results for startups and enterprises alike.
                         </p>
                     </div>
+                    <div>
+                        <span className="font-bold">Filter Projects:</span>&nbsp;&nbsp;
+                        <select className="border-2 rounded-md p-2 min-w-[250px]" ref={inputRef} onChange={changeHandler}>
+                            { portfolioOptions.map((item: selectOptionProps) => (<option key={item.value} value={item.value}>{item.title}</option>))}
+                        </select>
+                    </div>
 
                     {/* <!-- Portfolio Grid --> */}
                     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8" id="portfolio-grid">
-                        {/* <!-- Web Development Projects --> */}
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="web">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="" className="rounded-md" src="/assets/images/portfolio-1.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">TLT Sports specializes in using research based interventions to maximize the success of our athletes, and in the integration of ABA, physical fitness and team sports for all players.</p>
-                            </div>
-                        </div>
-
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="web">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-2.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">In 1973, husband and wife Stanley and Marilyn Edelstein founded ECHOtape with a blue Volkswagen and a single roll of packing tape. With Stanley’s background in engineering and experience in the tape industry, they moved to Montreal, Marilyn’s hometown, to start the company naming it Edelstein Diversified</p>
-                            </div>
-                        </div>
-
-                        {/* <!-- Mobile Development Projects --> */}
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="mobile">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-3.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">Having been a Florida vacation home investment owner and manager, Heather understands the intricacies of short-term rental niche market in the Disney area. Both her education & global experiences living abroad have enhanced her naturally detail-oriented personality and cultural awareness.</p>
-                            </div>
-                        </div>
-
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="mobile">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-4.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">Brewing gadgets mission is to be your go-to destination for all your brewing needs, offering top-quality, genuine products and exceptional customer service to enhance your coffee journey.</p>
-                            </div>
-                        </div>
-
-                        {/* <!-- CMS Projects --> */}
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="cms">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-5.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">Founded in 2007 by Deepak Aggarwal, KAZO is a premier Indian western-wear brand that brings affordable international fashion to the modern, confident woman . With over 170+ stores across 70+ cities.</p>
-                            </div>
-                        </div>
-
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="cms">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-6.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">Rooted in India’s heritage of mastery and draping, Tarun Tahiliani has created the vision of “India Modern” by merging centuries-old craftsmanship with refined global tailoring
-                                    . Since founding his Design Studio in 1995—following the success of Ensemble, India’s first luxury multi-designer boutique in 1987—his couture </p>
-                            </div>
-                        </div>
-
-                        {/* <!-- SEO Projects --> */}
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="seo">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" src="/assets/images/portfolio-7.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">Founded in 2015, VAPO was established to deliver premium, high-quality e-cigarette products that differed from those available on the market at the time. Since launching our first brick-and-mortar store in Auckland in 2017, we’ve grown both our store footprint and reputation as a reliable and responsible vape shop.</p>
-                            </div>
-                        </div>
-
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="seo">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-8.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">Founded in 2016 by Simran Lal and Raul Rai, Nicobar is a mindful, design-led Indian lifestyle brand that blends heritage and sustainability with modern minimalism . With a thoughtfully curated range—spanning apparel, homeware, gifts, and travel essentials—the brand draws inspiration from Indian Ocean journeys and local craftsmanship
-                                    .</p>
-                            </div>
-                        </div>
-
-                        {/* <!-- CRM Projects --> */}
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="crm">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" className="rounded-md" src="/assets/images/portfolio-9.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">With over 30 years of legacy, Maharishi Ayurveda India stands as North India’s first NABH-accredited Ayurvedic institution, blending authentic Vedic practices—like Panchakarma, Vedic diet, and meditation—with modern research-backed wellness programs
-                                    . Their Delhi hospital and Tapovan retreat in Rishikesh offer global guests a holistic path to health, rooted in the balance of mind, body, and spirit
-                                    .</p>
-                            </div>
-                        </div>
-
-                        <div className="portfolio-card bg-white rounded-xl shadow-lg overflow-hidden" data-category="crm">
-                            <div className="min-h-48 flex items-center justify-center p-1">
-                                <Image height={240} width={432} alt="Portfolio" src="/assets/images/portfolio-10.jpg" />
-                            </div>
-                            <div className="p-6">
-                                <p className="text-gray-600 mb-4">In 2021, our founder, Natasha Jamal, embarked on a mission to offer a period solution that would disrupt the industry and we’ve spent the last two years engineering, designing and testing a product that’s utterly fantastic and completely revolutionary—Mahina.</p>
-                            </div>
-                        </div>
+                        {
+                            filteredList && filteredList.map(portfolio => (<PortfolioCard key={portfolio.id} portfolio={portfolio} showInfo={false} />)
+                            ) 
+                        }
                     </div>
                 </div>
             </section>
